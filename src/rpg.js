1c9d22ae716e34e378336cf0c5571dedb17ee716
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-15 11:26:26 -0700

    Add storeState and changeState for managing state
    Co-authored-by: jeremy kale padot <jeremy@padot.us>
    Co-authored-by: Jonathan Carlos <jonathancarlos21@hotmail.com>
    Co-authored-by: Geoff Goetz <geoff.goetz503@gmail.com>

--- src/rpg.js
+++ src/rpg.js
@@ -1,1 +1,61 @@
-console.log("hello world");
+/*
+game = {
+
+  character = {
+    health: 100,
+    potions: 1,
+    items: []
+  },
+  map = [room1, room2, room3];
+}
+
+takeHit => character.health -10;
+drinkPotion => character.health +50;
+acquireItem(item) => character.items.push(item);
+
+1. create character
+2. creating enemy
+3. combat
+  - damaging character
+  - damaging enemy
+  - taking potions
+  - rewards
+
+*/
+export const storeState = (initialState) => {
+  let currentState = initialState;
+  return(stateChangeFunction) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = {...newState};
+    return newState;
+  }
+}
+
+export const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop]: (state[prop] || 0) + value
+    });
+  }
+}
+
+// const game = storeState(initialGameState);
+
+
+export const player = changeState("health");
+const player = storeState({health: 100, potions: 2});
+export const hurtPlayer = player("health")(-10);
+export const takePotion = player("health")(20);
+export const usePotion = player("potions")(-1);
+export const recievePotion = player("potions")(1);
+
+
+// console.log(game);
+// console.log(hurtPlayer(game));
+
+
+// const hp = changeState("health");
+// const startingHp = hp(100);
+// const takeDamage = hp(-10);
+// const takePotion = hp(20);
